cd034ea009d5af42f6648c379423184ac1435ec7
Govind <govindkumarchoudhary333@gmail.com>
2025-08-17 17:08:16 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryNewApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
